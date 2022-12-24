dec58dcfe9e70ba5190c80e25fe623b2447b85ef
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-24 13:18:23 -0300

    aula 3.5 mostrando mensagem na tela

--- app/recvoz.js
+++ app/recvoz.js
@@ -1,9 +1,20 @@
+const elementoChute = document.getElementById('chute')
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-recognition.lang='pt-Br'
+recognition.lang = 'pt-Br'
 recognition.start()
 recognition.addEventListener("result", onSpeak)
 
-function onSpeak(e){
-    console.log(e.results[0][0].transcript)
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChute(chute)
+}
+
+function exibeChute(chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${chute}</span>
+    <div>o número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+    `
 }
