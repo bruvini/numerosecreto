b3a8d730e15be8733a5d79dbed7996f9bd97f696
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-24 12:56:09 -0300

    aula 2.3 maninpulando maior e menor valor

--- app/sorteio.js
+++ app/sorteio.js
@@ -1,4 +1,15 @@
+/**CONSTANTES*/
+const menorValor = 1
+const maiorValor = 1000
 const numeroSecreto = gerarNumero()
+const menorElemento = document.getElementById('menor-valor')
+const maiorElemento = document.getElementById('maior-valor')
+
+/**FUNÇÕES*/
 function gerarNumero() {
-    return parseInt(Math.random()*101)
+    return parseInt(Math.random() * maiorValor + 1)
 }
+
+/**EVENTOS */
+menorElemento.innerHTML = menorValor
+maiorElemento.innerHTML = maiorValor
