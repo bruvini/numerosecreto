8c1b2299e2feb9e8294f9224344ecea41ac23fa8
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-24 13:40:21 -0300

    aula 4.3 acertando numero secreto

--- app/recvoz.js
+++ app/recvoz.js
@@ -17,6 +17,5 @@ function exibeChute(chute) {
     elementoChute.innerHTML = `
     <div>Você disse:</div>
     <span class="box">${chute}</span>
-    <div>o número secreto é maior <i class="fa-solid fa-up-long"></i></div>
     `
 }
--- app/validacao.js
+++ app/validacao.js
@@ -1,11 +1,18 @@
 function verificaChute(chute){
     const numero = +chute
     if (chuteInvalido(numero)) {
-        
+        elementoChute.innerHTML += `<div>Valor Inválido</div>` 
     }
 
     if (foraParametro(numero)) {
-        
+        elementoChute.innerHTML += `<div>Número inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>`
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Parabéns, você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
     }
 }
 
